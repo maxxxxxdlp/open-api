ce2d2cf3fdfff5080ed0c97b63e0a0c9f8eed4e4
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-01-26 23:27:30 +0200

    Fix error reporting not working

--- src/gui_wrapper/src/api.py
+++ src/gui_wrapper/src/api.py
@@ -1,6 +1,13 @@
 import requests
-
+from src.utils import report_error
+import json
 
 def fetch_response(request_url: str) -> str:
 	request = requests.get(request_url)
+	if request.status_code != 200:
+		report_error(json.dumps({
+			'status_code': request.status_code,
+			'url': request_url,
+			'text': request.text,
+		}, indent=4))
 	return request.text
