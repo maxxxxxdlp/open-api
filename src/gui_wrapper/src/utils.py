3a035d24df5283d0072a6bf8e346a167dc121c99
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-01-26 23:27:30 +0200

    Fixes error reporting not working

--- src/gui_wrapper/src/utils.py
+++ src/gui_wrapper/src/utils.py
@@ -1,14 +1,14 @@
 import os
 from datetime import datetime
-from src.gui_wrapper import settings
+import settings
 
 
-def report_error(content) -> None:
+def report_error(content:str) -> None:
 
 	if not os.path.exists('my_folder'):
 		os.makedirs('my_folder')
 
 	date_time_now = datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
 
-	with open(os.path.join(settings.ERROR_LOGS_LOCATION, date_time_now) ,'wb') as file:
+	with open(os.path.join(settings.ERROR_LOGS_LOCATION, date_time_now) ,'w') as file:
 		file.write(content)
