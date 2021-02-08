316c64c537103d2bb4689a01362b2327c9336875
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-02-08 20:56:09 +0200

    Delete redundant API schema validator

--- src/LmRex/automated_testing/validate_openapi_schema/index.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from LmRex.config import frontend_config as settings
-from openapi_spec_validator import validate_spec
-
-with open(settings.OPEN_API_LOCATION) as file:
-	spec_dict = yaml.safe_load(file.read())
-
-validate_spec(spec_dict)
