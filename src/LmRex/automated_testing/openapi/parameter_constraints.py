4f7f019d6ec93d10974c5ae3b00863a8d40f00d1
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-02-08 20:58:27 +0200

    Change indentation from tabs to spaces

--- src/LmRex/automated_testing/openapi/parameter_constraints.py
+++ src/LmRex/automated_testing/openapi/parameter_constraints.py
@@ -2,35 +2,35 @@
 from typing import Callable, List, Dict
 
 
-def get_gbif_records(path: str, response: Dict[str,any]) -> List[Dict[str,any]]:
-	return response['records'] if 'gbif' in path else response['records'][0]['GBIF']['records']
+def get_gbif_records(path: str, response: Dict[str, any]) -> List[Dict[str, any]]:
+    return response['records'] if 'gbif' in path else response['records'][0]['GBIF']['records']
 
 
-def gbif_count(parameter_value: bool, path: str, response: Dict[str,any]) -> bool:
-	for record in get_gbif_records(path, response):
-		if (
-				not parameter_value and ('occurrence_count' in record or 'occurrence_url' in record)
-		) or \
-				(
-						parameter_value and ('occurrence_count' not in record or 'occurrence_url' not in record)
-				):
-			return False
-	return True
+def gbif_count(parameter_value: bool, path: str, response: Dict[str, any]) -> bool:
+    for record in get_gbif_records(path, response):
+        if (
+                not parameter_value and ('occurrence_count' in record or 'occurrence_url' in record)
+        ) or \
+                (
+                        parameter_value and ('occurrence_count' not in record or 'occurrence_url' not in record)
+                ):
+            return False
+    return True
 
 
-def gbif_accepted(parameter_value: bool, path: str, response: Dict[str,any]) -> bool:
-	for record in get_gbif_records(path, response):
-		if parameter_value and record['status'] != 'ACCEPTED':
-			return False
-	return True
+def gbif_accepted(parameter_value: bool, path: str, response: Dict[str, any]) -> bool:
+    for record in get_gbif_records(path, response):
+        if parameter_value and record['status'] != 'ACCEPTED':
+            return False
+    return True
 
 
 # TODO: finish this one once `occ` endpoints are fixed
-def count_only(parameter_value: bool, path: str, response: Dict[str,any]) -> bool:
-	pass
+def count_only(parameter_value: bool, path: str, response: Dict[str, any]) -> bool:
+    pass
 
 
-parameter_constraints: dict[str, Callable[[bool, str, Dict[str,any]], bool]] = {
-	'gbif_count':    gbif_count,
-	'gbif_accepted': gbif_accepted,
+parameter_constraints: dict[str, Callable[[bool, str, Dict[str, any]], bool]] = {
+    'gbif_count':    gbif_count,
+    'gbif_accepted': gbif_accepted,
 }
