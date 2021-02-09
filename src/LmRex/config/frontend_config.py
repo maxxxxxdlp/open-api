4299d2bb94a7b5c8e763b6a4ba60a15186bc9ceb
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-02-09 21:28:24 +0200

    Create a GUI for collection stats

--- src/LmRex/config/frontend_config.py
+++ src/LmRex/config/frontend_config.py
@@ -10,3 +10,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, '../frontend/static/templates')
 OPEN_API_LOCATION = os.path.join(BASE_DIR, 'open_api.yaml')
 
 ERROR_LOGS_LOCATION = os.path.join(BASE_DIR, '../automated_testing/error_logs')
+
+COLLECTION_STATS_BASE_DIR = os.path.join(BASE_DIR, '../collection_stats')
+
+COLLECTION_STATS_RELATIVE_LOCATION = 'data'
+
+COLLECTION_STATS_LOCATION = os.path.join(
+    BASE_DIR,
+    '../collection_stats/%s' % COLLECTION_STATS_RELATIVE_LOCATION
+)
