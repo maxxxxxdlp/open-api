45e2b07cbec2d52d8e734630db44f7b768d48402
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-03-19 15:22:01 +0200

    Make code comply with `flake8`

--- src/common/parse_schema.py
+++ src/common/parse_schema.py
@@ -1,5 +1,6 @@
 import yaml
 from openapi3 import OpenAPI
+
 from src import config
 
 with open(config.OPEN_API_LOCATION) as file:
--- src/common/report_error.py
+++ src/common/report_error.py
@@ -7,25 +7,25 @@ from src import config
 
 
 def report_error(content: Dict[str, any]) -> None:
-  """
-  Save the error message to the logs folder
-  Args:
-      content: the error message
+    """
+    Save the error message to the logs folder
+    Args:
+        content: the error message
 
-  Returns:
-      None
-  """
-  if not os.path.exists(config.ERROR_LOGS_LOCATION):
-    os.makedirs(config.ERROR_LOGS_LOCATION)
+    Returns:
+        None
+    """
+    if not os.path.exists(config.ERROR_LOGS_LOCATION):
+        os.makedirs(config.ERROR_LOGS_LOCATION)
 
-  date_time_now = datetime.now().strftime("%d_%m_%Y__%H_%M_%S.json")
+    date_time_now = datetime.now().strftime("%d_%m_%Y__%H_%M_%S.json")
 
-  formatted_error = json.dumps(content, indent=4, default=str)
+    formatted_error = json.dumps(content, indent=4, default=str)
 
-  with open(
-    os.path.join(
-      config.ERROR_LOGS_LOCATION, date_time_now
-    ),
-    'w'
-  ) as file:
-    file.write(formatted_error)
+    with open(
+        os.path.join(
+            config.ERROR_LOGS_LOCATION, date_time_now
+        ),
+        'w'
+    ) as file:
+        file.write(formatted_error)
